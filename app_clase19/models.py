337016e61ee8aa4f3cd5eccfca0cab1a0424a034
Tomas Carranza Casares <tomicarranza999@gmail.com>
2022-07-01 03:01:58 -0300

    commit

--- app_clase19/models.py
+++ app_clase19/models.py
@@ -5,3 +5,9 @@ class Productos(models.Model):
     producto = models.CharField(max_length = 40)
 
     cantidad = models.IntegerField()
+
+# al instanciar la clase y crear objetos nuevos, esos objetos donde almaceno la instanciacion, pasan a ser
+# objetos de tipo: class, en este caso de tipo: Productos
+# entonces en el template, en: {% for elemento in productos %}
+# la variable de control: elemento, por cada elemento(objeto) que tome dentro de la lista de objetos que se encuentra
+# comovalor de la clave: productos, sera tambien un objeto de tipo: class tambien
